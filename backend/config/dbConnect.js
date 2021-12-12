755c47b73e84071f66793e7a1aa28332cbcbe9b5
VijaiNatesh <glennvijay@gmail.com>
2021-12-12 22:29:06 +0500

    inventory MERN Stack App
    first commit

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const dbConnect = async () => {
+  try {
+   await mongoose.connect(
+      "mongodb+srv://vijay:vijay123123@cluster0.76a7k.mongodb.net/inventory-bill-app",
+      {
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+      },
+      () => {
+        console.log('DB connected');
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = dbConnect;
+
